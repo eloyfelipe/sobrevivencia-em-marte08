8ebaef6846d5d14022d84638f1989dba3ecd6823
I1HN <eloyfel@outlook.com>
2026-03-18 09:08:45 -0300

    coreçao do codigo

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let oxigenio = (prompt("Insira a quantidade de oxigenio restante(em livros):"));
 let pessoas = Number(prompt("Insira a quatidade de pessoas em marte:"));
-Const consumoDiario = 15; // litros por pessoa.
+const consumoDiario = 15; // litros por pessoa.
 // consumo total é baseada na quantidade de pessoas multiplicado pelo consumo diario por pessoa.
 
 let consumoTotalBase = pessoas * consumoDiario;
 let diasRestantes = oxigenio / consumoTotalBase;
 
-alert(`Relatório de Marte: restam ${diasRestantes.toFixed(0)} dias de oxigenio!! `);
+alert(`Relatório de Marte: restam ${diasRestantes.toFixed(0)} dias de oxigenio!! `)
